d338a2500fbb1bfaf493301908c77492853940bf
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 14:53:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ if file is not None:
             roi_gray = cv2.resize(roi, (48, 48), interpolation=cv2.INTER_AREA)
             roi_gray = roi_gray.astype('float') / 255.0
             roi_gray = np.expand_dims(roi_gray, axis=0)
-            prediction = model.predict(roi_gray)[0]
+            prediction = model.predict(roi_gray)
             label = emotion_labels[prediction.argmax()]
             label_position = (x, y - 10)
             cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
