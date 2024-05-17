73ebe0998cdcb11414f3d1796c3183e8e7fded26
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 00:26:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,17 +17,17 @@ emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surpri
 if file is not None:
     face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     st.image(image, channels="BGR", caption='Original Image')
 
     model = load_model('model (1).h5')
 
-    img = cv2.resize(gray_image, (48, 48), interpolation=cv2.INTER_AREA)
-    img_np = np.asarray(img)
-    img_reshape=img_np[np.newaxis,...]
-    prediction = model.predict(img_reshape)
+    image=Image.open(file)
+    size=(48,48)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+
 
 
 
