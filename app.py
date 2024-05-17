af3336d655f0321240af57484fa1b53a43b7674e
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 00:20:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if file is not None:
 
     model = load_model('model (1).h5')
 
-    cv2.resize(roi, (48, 48), interpolation=cv2.INTER_AREA)
+    cv2.resize(gray_image, (48, 48), interpolation=cv2.INTER_AREA)
     img=np.asarray(roi_gray)
     img_reshape=img[np.newaxis,...]
     prediction = model.predict(img_reshape)
