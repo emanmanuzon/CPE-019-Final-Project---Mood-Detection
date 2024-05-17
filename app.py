68c213041097562afa2950b586483637f3c7825b
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 00:10:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,47 +14,5 @@ file = st.file_uploader("Choose photo from the computer", type=["jpg", "png"])
 
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
-if file is not None:
-    # Load the pre-trained face detector
-    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # Read the uploaded image
-    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
-
-    # Convert the image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the grayscale image
-    faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # Draw rectangles around the detected faces and extract ROIs
-    rois = []
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi = image[y:y + h, x:x + w]
-        rois.append(roi)
-
-    # Display the original image
-    st.image(image, channels="BGR", caption='Original Image')
-
-    # Display the image with detected faces
-    st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected')
-
-    if rois:
-        # Load the pre-trained emotion classifier model
-        model = load_model('model (1).h5')
-
-        # Make predictions for each detected face
-        for roi in rois:
-            roi_gray = cv2.resize(roi, (48, 48), interpolation=cv2.INTER_AREA)
-            img=np.asarray(roi_gray)
-            img_reshape=img[np.newaxis,...]
-            string="OUTPUT : "
-            st.success(string)
-            prediction = model.predict(img_reshape)
-            label = emotion_labels[prediction.argmax()]
-            label_position = (x, y - 10)
-            cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        # Display the image with predicted emotions
-        st.image(image, channels="BGR", caption='Image with Predicted Emotions')
+    
