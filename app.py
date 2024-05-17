db14729824e08fb716067f6ef657d13b006ea6cb
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import tensorflow as tf
 from keras.models import load_model
 
-model = load_model('model (1).h5')
+@st.cache_data(experimental_allow_widgets=True)
+def load_model():
+  model=tf.keras.models.load_model('model (1).h5')
+  return model
 st.write("""
 # Mood Classifier"""
 )
