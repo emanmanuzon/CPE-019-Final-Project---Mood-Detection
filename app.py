c261f8a6bd77ac3157360fc586022ea0a010a6fa
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:46:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import tensorflow as tf
 
+model = load_model('emotion_model1.h5')
+
 st.write("""
-# Weather Detection System"""
+# Mood Classifier App"""
 )
 file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 
