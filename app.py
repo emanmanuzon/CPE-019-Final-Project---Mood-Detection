cb4fce7a6af8955f5b9f95ededd3e59707e43841
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 00:59:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from keras.models import load_model
 
 @st.cache_data(experimental_allow_widgets=True)
 def load_model():
-  model=tf.keras.models.load_model('emotion_model1.h5')
+  model=tf.keras.models.load_model('CNN_Model_7.h5')
   return model
 model=load_model()
 
