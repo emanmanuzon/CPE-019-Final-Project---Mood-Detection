d7a17b1e6e98ca3ca8bbc6e1b7edbe642cd679f9
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 00:16:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ file = st.file_uploader("Choose photo from the computer", type=["jpg", "png"])
 
 emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
+if file is not None:
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    st.image(image, channels="BGR", caption='Original Image')
+
+
+
+
 
     
