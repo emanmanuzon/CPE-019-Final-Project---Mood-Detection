3c807946b6976edc2dc2b58fa32932e29c4954bb
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:53:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from keras.models import load_model
 
 model = load_model('model (1).h5')
 st.write("""
-# Weather Detection System"""
+# Mood Classifier"""
 )
-file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose photo from the compu",type=["jpg","png"])
 
