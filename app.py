a901ae044b87448fd7540205fa4eaea21ca67504
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:48:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 from keras.models import load_model
 
-model = load_model('emotion_model1.h5')
+model = load_model('CNN_Model_7.h5')
 st.write("""
 # Weather Detection System"""
 )
