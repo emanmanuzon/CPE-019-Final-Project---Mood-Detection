35cecd1ff9eb0ec16fd5019920d795372d2d7243
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 01:36:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.write("""
 file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 
 image = Image.open(file)
-image = image.resize((224, 224))  # Resize to the input size the model expects
+image = image.resize((48, 48))  # Resize to the input size the model expects
 image = img_to_array(image)  # Convert the image to an array
 image = np.expand_dims(image, axis=0)  # Add batch dimension
 image = image / 255.0  # Normalize the image if the model expects normalized input
