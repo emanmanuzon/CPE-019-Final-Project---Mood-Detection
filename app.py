488ccb7b3c37dee9e636ed5ee606e2ca3abce6ef
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:59:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,26 @@ st.write("""
 )
 file=st.file_uploader("Choose photo from the computer",type=["jpg","png"])
 
+def detect_faces(image):
+    # Load the pre-trained face detector
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Detect faces in the grayscale image
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    # Draw rectangles around the detected faces and extract ROIs
+    rois = []
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        roi = image[y:y+h, x:x+w]
+        rois.append(roi)
+    
+    return image, len(faces), rois
+
+
 if file is not None:
     # Read the uploaded image
     image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)  # Use np.frombuffer to handle binary data
