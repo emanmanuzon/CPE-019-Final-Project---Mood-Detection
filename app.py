ef7aef1070bfd5c0cb1826bad54d5a05f90097f3
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-17 13:47:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import tensorflow as tf
+from keras.models import load_model
 
-model = load_model('emotion_model1.h5')
-
+@st.cache_data(experimental_allow_widgets=True)
+def load_model():
+  model=tf.keras.models.load_model('emotion_model1.h5')
+  return model
+model=load_model()
 st.write("""
-# Mood Classifier App"""
+# Weather Detection System"""
 )
 file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 
