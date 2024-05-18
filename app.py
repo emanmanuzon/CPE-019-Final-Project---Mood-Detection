6fd02215b328bf6b425cd32ae3c269bd03e38321
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 10:33:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import streamlit as st
 import tensorflow as tf
-import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from keras.optimizers import Adam
-from PIL import Image
 
-
-model=load_model('CNN_Model_7.h5')
-
+@st.cache_data(experimental_allow_widgets=True)
+def load_model():
+  model=tf.keras.models.load_model('CNN_Model_7.h5')
+  return model
+model=load_model()
 st.write("""
-# Mood Classifier"""
+# Weather Detection System"""
 )
+file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 
-file = st.file_uploader("Choose photo from the computer", type=["jpg", "png"])
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(48,48)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Cloudy', 'Rain', 'Shine', 'Sunrise']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
