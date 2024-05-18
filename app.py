5e041b9ebb76d72f310f2938f880a53a0fdfae66
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 11:54:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model=load_model()
 st.write("""
 # Mood Classifier"""
 )
-file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
+file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
 
 import cv2
 from PIL import Image,ImageOps
@@ -17,18 +17,19 @@ import numpy as np
 if file is None:
     st.text("Please upload an image file")
 else:
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
+    if st.button("Detect Faces and Predict Mood"):
+      face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+      image = cv2.imdecode(np.frombuffer(file.read(), np.uint8), -1)
 
-    st.image(image, channels="BGR", caption='Original Image')
+      st.image(image, channels="BGR", caption='Original Image')
 
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+      faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    mood_labels = ['Angry', 'Happy', 'Neutral', 'Sad']
+      mood_labels = ['Angry', 'Happy', 'Neutral', 'Sad']
     
-    for (x, y, w, h) in faces:
+      for (x, y, w, h) in faces:
         roi = image[y:y + h, x:x + w]
         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)  # Convert to RGB
         roi_pil = Image.fromarray(roi)
@@ -45,15 +46,4 @@ else:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected and labeled')
-    
-    #image=Image.open(file)
-    #st.image(image,use_column_width=True)
-    #size=(48,48)
-    #image=ImageOps.fit(image,size)
-    #img=np.asarray(image)
-    #img_reshape=img[np.newaxis,...]
-    #prediction=model.predict(img_reshape)
-    #class_names=['Angry', 'Happy', 'Neutral', 'Sad']
-    #string="OUTPUT : "+class_names[np.argmax(prediction)]
-    #st.success(string)
+      st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected and labeled')
