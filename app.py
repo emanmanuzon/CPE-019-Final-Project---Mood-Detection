5d78dcbc8e2b8279cb59ecdc9e3204e0feb7dc78
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 09:43:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from keras.optimizers import Adam
 from PIL import Image
 
 
 model=tf.keras.models.load_model('CNN_Model_7.h5')
+model.compile(loss='categorical_crossentropy',
+              optimizer = Adam(lr=0.001),
+              metrics=['accuracy'])
 st.write("""
 # Mood Classifier"""
 )
