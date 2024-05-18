08b178336ade34c01c2e02123a9a3c3f533fc876
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 11:10:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,13 @@ else:
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected')
-    
     rois = []
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi = image[y:y + h, x:x + w]
         rois.append(roi)
+
+    st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected')
 
     #image=Image.open(file)
     #st.image(image,use_column_width=True)
