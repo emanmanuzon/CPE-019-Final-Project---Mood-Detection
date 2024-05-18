7c5af7f4a6b709d70f9d9c340060713b46cbdad6
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 11:09:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ else:
 
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
     st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected')
     
