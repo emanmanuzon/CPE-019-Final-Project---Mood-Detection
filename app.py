e1df834542d3e0ffd61e94b7edd0c5323dbe6262
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 13:41:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,7 @@ else:
             st.image(image, channels="BGR", caption=f'Image with {len(faces)} face(s) detected and labeled')
 
             st.write("Summary of Detected Faces:")
-            mood_counts = {mood: detected_faces.count(mood) for mood in mood_labels}
+            mood_counts = {mood: 0 for mood in mood_labels}  # Initialize mood counts with zeros
+            for mood in detected_faces:
+                mood_counts[mood] += 1
             st.table(mood_counts)
