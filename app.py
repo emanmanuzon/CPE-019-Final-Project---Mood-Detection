676cc12866fd1345400b5e117c73a2f78cbe3907
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 09:14:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,13 @@ model = load_keras_model()
 st.write("""
 # Weather Detection System"""
 )
+
 file=st.file_uploader("Choose weather photo from computer",type=["jpg","png"])
 
-image = Image.open(file)
-image = image.resize((48, 48))  # Resize to the input size the model expects
-image = img_to_array(image)  # Convert the image to an array
-image = np.expand_dims(image, axis=0)  # Add batch dimension
-image = image / 255.0  # Normalize the image if the model expects normalized input
-predictions = model.predict(image)
+If file is not None:
+    image = Image.open(file)
+    image = image.resize((48, 48))  # Resize to the input size the model expects
+    image = img_to_array(image)  # Convert the image to an array
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    image = image / 255.0  # Normalize the image if the model expects normalized input
+    predictions = model.predict(image)
