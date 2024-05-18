5d7fb25933e00bd2a6f1559ba2fecdd975e148f1
emanmanuzon <167847278+emanmanuzon@users.noreply.github.com>
2024-05-18 09:28:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from PIL import Image
 
-def load_keras_model():
-    model = load_model('CNN_Model_7.h5')
-    return model
-model = load_keras_model()
+@st.cache_data(experimental_allow_widgets=True)
+def load_model():
+  model=tf.keras.models.load_model('CNN_Model_7.h5')
+  return model
+model=load_model()
+
 st.write("""
 # Mood Classifier"""
 )
